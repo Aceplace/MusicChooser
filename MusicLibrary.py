73ece490f02c3a15efac7fd3916004a49970dde2
Michael Yeiter <Aceplace1116@gmail.com>
2019-01-01 16:55:03 -0500

    Load library and update library.

--- MusicLibrary.py
+++ MusicLibrary.py
@@ -32,4 +32,29 @@ def save_library(library, file_name):
     with open(file_name, 'w') as file:
         json.dump(library, file, indent=4)
 
-save_library(create_library(library_path), 'library.json')
+def load_library(file_name):
+    with open(file_name, 'r') as file:
+        library = json.load(file)
+    return library
+
+def get_updated_library(old_library, library_path):
+    library = create_library(library_path)
+
+    #go through old library and copy priorities over
+    for category in old_library['categories'].keys():
+        if category in library['categories'].keys():
+            for song in old_library['categories'][category]:
+                copy_over_priority(song, library['categories'][category])
+
+    return library
+
+def copy_over_priority(song_to_copy_priority, category_songs):
+    songs_to_modify_priority = [song for song in category_songs
+                                if song['artist_name']==song_to_copy_priority['artist_name']
+                                and song['song_name']==song_to_copy_priority['song_name']]
+
+    for song in songs_to_modify_priority:
+        song['priority'] = song_to_copy_priority['priority']
+
+def pick_song_from_library(library, priority_weights, repeat_list):
+    pass

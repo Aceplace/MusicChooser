05c4aab69b2cace93355f2c9d26e31ae0af815ca
Michael Yeiter <Aceplace1116@gmail.com>
2019-01-02 16:23:28 -0500

    Added weighting functionality.

--- MusicLibraryEditor.py
+++ MusicLibraryEditor.py
@@ -1,7 +1,8 @@
 import os
+import traceback
 import tkinter as tk
 from tkinter import filedialog, messagebox
-import MusicLibrary as mL
+import MusicLibrary as ml
 
 
 class App(tk.Tk):
@@ -10,6 +11,8 @@ class App(tk.Tk):
         self.library = None
         self.current_library_filename = None
         self.current_category = None
+        self.weight_validate_command = (self.register(self.weight_validate), '%P', '%W')
+        self.loading_weights = False
 
         # Menu
         menu_bar = tk.Menu(self, tearoff=False)
@@ -21,19 +24,21 @@ class App(tk.Tk):
         self.config(menu=menu_bar)
 
         # Library Frame
-        self.library_frame = tk.Frame(self)
-        self.library_frame.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
-        self.library_frame.rowconfigure(3, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
+        library_frame = tk.Frame(self)
+        library_frame.grid(row=0, column=0, sticky='NWS')
+        library_frame.rowconfigure(3, weight=1)
 
         # Library Frame -> Category Chooser
-        tk.Label(self.library_frame, text='Categories').grid(row=0, column=0)
-        self.category_om = tk.OptionMenu(self.library_frame, tk.StringVar(), 'NONE')
+        tk.Label(library_frame, text='Categories').grid(row=0, column=0)
+        self.category_om = tk.OptionMenu(library_frame, tk.StringVar(), 'NONE')
         self.category_om.grid(row=1, column=0)
 
         # Library Frame -> Song Name Listbox
-        tk.Label(self.library_frame, text='Song Names').grid(row=2, column=0)
+        tk.Label(library_frame, text='Song Names').grid(row=2, column=0)
 
-        song_name_lb_frame = tk.Frame(self.library_frame)
+        song_name_lb_frame = tk.Frame(library_frame)
         song_name_lb_frame.grid(row=3, column=0, sticky='NS')
         song_name_lb_frame.grid_rowconfigure(0, weight=1)
 
@@ -49,22 +54,61 @@ class App(tk.Tk):
         y_scroll.config(command=self.song_name_lb.yview)
 
         # Library Frame -> Set Priorities
-        priorities_frame = tk.Frame(self.library_frame)
+        priorities_frame = tk.Frame(library_frame)
         priorities_frame.grid(row=3, column=1, sticky='NW')
         tk.Label(priorities_frame, text='Set Priorities:').pack()
         for i in range(10):
             tk.Button(priorities_frame, text=f' {i} ', command=lambda p=i: self.set_priority(p)).pack()
+
+        # Priority Weights Frame
+        priority_weights_frame = tk.Frame(self)
+        priority_weights_frame.grid(row=0, column=1, sticky='NW')
+
+        tk.Label(priority_weights_frame, text='Priority Weights').grid(row=0, column=0)
+        self.weight_modifier_widgets = []
+        for i in range(10):
+            entry_var = tk.StringVar()  # Used to detect changes to weight entries and automatically update stuff
+            entry_var.trace_add('write', self.weight_entry_callback)
+            weight_entry = tk.Entry(priority_weights_frame, textvariable=entry_var, validate="key",
+                                    validatecommand=self.weight_validate_command)
+            entry_var.set('0')
+            weight_label = tk.Label(priority_weights_frame, text=f'Priority: {i}\tWaiting for Loaded Library')
+            weight_entry.grid(row=i+1, column=0)
+            weight_label.grid(row=i+1, column=1, sticky='W')
+            self.weight_modifier_widgets.append((weight_entry, weight_label, entry_var))
+
+    def weight_entry_callback(self, *args):
+        if self.loading_weights:
+            return
+        if self.library:
+            self.library['weights'] = [int(weight_widgets[0].get()) for weight_widgets in self.weight_modifier_widgets]
+            self.refresh_weight_labels()
+
+
+    def refresh_weight_labels(self):
+        relative_frequencies = ml.calculate_relative_frequency(self.library, 40)
+        for priority, weight_label in enumerate(list(zip(*self.weight_modifier_widgets))[1]):
+            if relative_frequencies[priority] == '--':
+                weight_label_text = f'Priority: {priority}'
+            else:
+                weight_label_text = 'Priority: {} A song will appear in 1 out of every {:.2f} playlists'.format(priority,
+                                                                                    float(relative_frequencies[priority]))
+            weight_label.configure(text=weight_label_text)
+
 
     def load_library(self):
         try:
             library_filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select Music Library",
                                                           filetypes=(("json", "*.json"),))
             if library_filename:
-                self.library = mL.load_library(library_filename)
+                self.library = ml.load_library(library_filename)
                 self.current_library_filename = library_filename
-                self.refresh_category_om()
                 self.current_category = None
+                self.refresh_category_om()
+                self.refresh_song_lb(None)
+                self.load_weights()
         except Exception as e:
+            print(traceback.print_exc())
             messagebox.showerror('Open Library Error', e)
 
     def refresh_category_om(self):
@@ -82,6 +126,15 @@ class App(tk.Tk):
             self.song_name_lb.insert(tk.END, insert_string)
         self.song_name_lb.config(width=0)
 
+
+    def load_weights(self):
+        self.loading_weights = True # This prevents weight entry callback from modifying library while library is writing into it
+        for priority in range(10):
+            self.weight_modifier_widgets[priority][2].set(str(self.library['weights'][priority]))
+        self.loading_weights = False
+        self.refresh_weight_labels()
+
+
     def set_priority(self, priority):
         if not self.current_category:
             return
@@ -98,6 +151,30 @@ class App(tk.Tk):
         for i in selected_song_indexes:
             self.song_name_lb.selection_set(i)
         self.song_name_lb.yview_moveto(y_view_fraction)
+
+    def weight_validate(self, new_value, widget_name):
+        # pre validation
+        entry_widget = self.nametowidget(widget_name)
+        entry_widget.config(validate=None, validatecommand=None)
+        if len(new_value) == 0:
+            for priority, entry_widget_check in enumerate(list(zip(*self.weight_modifier_widgets))[0]):
+                if entry_widget is entry_widget_check:
+                    self.weight_modifier_widgets[priority][2].set('0')
+        elif len(new_value) == 2 and new_value[0]=='0' and new_value[1].isdigit():
+            for priority, entry_widget_check in enumerate(list(zip(*self.weight_modifier_widgets))[0]):
+                if entry_widget is entry_widget_check:
+                    self.weight_modifier_widgets[priority][2].set(new_value[1])
+        entry_widget.config(validate='key', validatecommand=self.weight_validate_command)
+
+        if len(new_value) > 1 and new_value[0]=='0':
+            return False
+        try:
+            int_value = int(new_value)
+            if int_value >= 0:
+                return True
+            return False
+        except ValueError:
+            return False
 
 
 

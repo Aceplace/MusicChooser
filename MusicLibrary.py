1b0df0e4cee8d7ef0ec19778e69fd35d0fccc7ba
Michael Yeiter <Aceplace1116@gmail.com>
2019-01-01 20:42:53 -0500

    Get priority and calculate relative frequencies.

--- MusicLibrary.py
+++ MusicLibrary.py
@@ -1,20 +1,24 @@
 import json
 import os
+import random
+from itertools import chain
+
 library_path = "E:\CoachYeiter Drive\Cheltenham2019\CheltenhamPlaylists2019\Library"
+
 
 def create_library(path):
     music_subdirectories = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
 
-    library = {'categories':{}}
+    library = {'categories': {}}
 
     for subdirectory in music_subdirectories:
         music_file_names = [file_path for file_path in os.listdir(os.path.join(path, subdirectory))
-                            if os.path.isfile(os.path.join(path,subdirectory,file_path))
+                            if os.path.isfile(os.path.join(path, subdirectory, file_path))
                             and os.path.splitext(file_path)[1] == '.mp3']
         library['categories'][subdirectory] = []
 
         for music_file_name in music_file_names:
-            music_file_path = os.path.abspath(os.path.join(path,subdirectory,music_file_name))
+            music_file_path = os.path.abspath(os.path.join(path, subdirectory, music_file_name))
             split_name = music_file_name.split('-')
             artist_name = split_name[0].strip()
             song_name = split_name[1].split('.')[0].strip()
@@ -28,14 +32,17 @@ def create_library(path):
 
     return library
 
+
 def save_library(library, file_name):
     with open(file_name, 'w') as file:
         json.dump(library, file, indent=4)
+
 
 def load_library(file_name):
     with open(file_name, 'r') as file:
         library = json.load(file)
     return library
+
 
 def get_updated_library(old_library, library_path):
     library = create_library(library_path)
@@ -48,13 +55,39 @@ def get_updated_library(old_library, library_path):
 
     return library
 
+
 def copy_over_priority(song_to_copy_priority, category_songs):
     songs_to_modify_priority = [song for song in category_songs
-                                if song['artist_name']==song_to_copy_priority['artist_name']
-                                and song['song_name']==song_to_copy_priority['song_name']]
+                                if song['artist_name'] == song_to_copy_priority['artist_name']
+                                and song['song_name'] == song_to_copy_priority['song_name']]
 
     for song in songs_to_modify_priority:
         song['priority'] = song_to_copy_priority['priority']
 
-def pick_song_from_library(library, priority_weights, repeat_list):
-    pass
+
+def pick_random_song_from_library(library, priority_weights, repeat_list):
+    priority = random.choices([priority_weight[0] for priority_weight in priority_weights],
+                              weights=[priority_weight[1] for priority_weight in priority_weights])
+    return priority
+
+
+def get_priority_number_of_songs(library):
+    priorities = {song['priority'] for song in chain(*library['categories'].values())}
+    song_priorities = [song['priority'] for song in chain(*library['categories'].values())]
+    priority_info = [(priority, song_priorities.count(priority)) for priority in priorities]
+    return priority_info
+
+
+def calculate_relative_frequency(priorities, categories_num_songs, weights, number_of_songs_in_playlist):
+    relative_frequencies = []
+    for priority, num_songs, weight in zip(priorities, categories_num_songs, weights):
+        relative_frequency = 1.0 / (weight / sum(weights) / num_songs) / number_of_songs_in_playlist
+        relative_frequencies.append((priority, relative_frequency))
+    print(relative_frequencies)
+
+
+library = create_library(library_path)
+priority_info = get_priority_number_of_songs(library)
+print(priority_info)
+calculate_relative_frequency(*zip(*priority_info),[100, 100, 100, 100, 100], 40)
+
